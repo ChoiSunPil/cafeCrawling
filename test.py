be02789367220c7d838b16bf4e69030450b6d8c3
choisunpil <roung4119@gmail.com>
2019-09-30 03:44:59 +0900

    메뉴 및 가격 ,  전화번호 크롤링 추가

--- test.py
+++ test.py
@@ -16,8 +16,19 @@ driver.get('https://store.naver.com/restaurants/detail?id=12952314')
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-notices = soup.select('div.ct_box_area > div.bizinfo_area > div.list_bizinfo > div.list_item.list_item_biztime > div.txt')
+# 전화 번호
+phone = soup.select('div.ct_box_area > div.bizinfo_area > div.list_bizinfo > div.list_item.list_item_biztel > div.txt')
 
+print("전화 번호 : ",phone[0].text.strip())
+
+# 개장 시간
+notices = soup.select('div.ct_box_area > div.bizinfo_area > div.list_bizinfo > div.list_item.list_item_biztime > div.txt')
 #print(list(notices))
 for n in notices:
+    print(n.text.strip())
+
+# 카페 메뉴
+menus = soup.select('div.ct_box_area > div.bizinfo_area > div.list_bizinfo > div.list_item.list_item_menu > div.txt')
+
+for n in menus :
     print(n.text.strip())
