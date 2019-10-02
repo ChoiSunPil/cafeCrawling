d1fc71fa5e2ebaf6c4e26e463eb6473a28467221
choisunpil <roung4119@gmail.com>
2019-10-02 19:19:23 +0900

    지하철 수정 완료, 문제점,tab 이동

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+# options.add_argument('headless')
 driver = webdriver.Chrome('./chromedriver',chrome_options =options)
 #자원 로드 될때 까지 기다림
 keyWord = input("카페 이름 상세히 입력해주세요 :") 
@@ -15,8 +15,12 @@ elem =driver.find_element_by_name('query').send_keys(keyWord)
 btn = driver.find_element_by_xpath('//*[@id="sform"]/fieldset/button').click()
 
 driver.find_element_by_xpath('//*[@id="place_main_ct"]/div/div/div[1]/div[3]/a').click()
+
+last_tab = driver.window_handles[-1]
+driver.switch_to.window(window_name=last_tab)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
+
 
 # 전화 번호
 phone = soup.select('div.ct_box_area > div.bizinfo_area > div.list_bizinfo > div.list_item.list_item_biztel > div.txt')
@@ -35,14 +39,13 @@ for n in menus :
     print(n.text.strip())
 
 # 지하철 역
-subways = soup.select('#panel01 > div > div.sc_box.contact > div.contact_area > div.transport_area > div > ul.list_station > li > div > div > a')
+subways = soup.select('#panel01 > div > div.sc_box.contact > div.contact_area > div.transport_area > div > ul:nth-child(2)> li > div > div > a')
 
 
 #얼마나 걸리는지
-t = soup.select('#panel01 > div > div.sc_box.contact > div.contact_area > div.transport_area > div > ul > li > div > div > span > span')
+t = soup.select('#panel01 > div > div.sc_box.contact > div.contact_area > div.transport_area > div > ul:nth-child(2) > li > div > div > span > span')
 for i in range(0,len(t)) :
     print("지하철 역",subways[i].text.strip(),", 도보 :",t[i].find_all('em')[0].text.strip())
     print("----------------------")
 
-
-# driver.quit()
+driver.quit()
